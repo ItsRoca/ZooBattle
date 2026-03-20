cf7c48bd9304cd818ca851e25cef52055cd85431
ItsRoca <adrianrocasantiago03@gmail.com>
2026-03-20 17:20:57 +0100

    Rehacer Reno
    No me convencía usar el sprite2D, así que volví a empezarlo pero con AnimatedSprite2D, ya que es el que usé en el trabajo del primer trimestre.

--- Scripts/Animales/unit_stats.gd
+++ Scripts/Animales/unit_stats.gd
@@ -1,4 +1,25 @@
+#Definimos la clase y la extension crear archivos .tres con estos datos desde el editor de Godot
 class_name UnitStats
 extends Resource
 
-enum Rarity {COMMON, UNCOMMON, RARE, LEGENDARY}
+#Indicamos los 4 tipos de rareza que estaran disponibles
+enum Rarity {COMMON, RARE, EPIC, LEGENDARY}
+
+const RARITY_COLORS := {
+	Rarity.COMMON: Color(0.169, 0.482, 0.149, 1.0),
+	Rarity.RARE: Color(0.251, 0.459, 0.871, 1.0),
+	Rarity.EPIC: Color(0.475, 0.149, 0.549, 1.0),
+	Rarity.LEGENDARY: Color(0.812, 0.533, 0.071, 1.0),
+}
+
+@export var name: String
+
+@export_category("Data")
+@export var rarity: Rarity
+@export var gold_cost := 1
+
+@export_category("Visuals")
+@export var skin_coordinates: Vector2i
+
+func _to_string() -> String:
+	return name
